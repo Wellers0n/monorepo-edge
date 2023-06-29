fc7217f0d46e38f540a6b57811bc89aa1d55abc5
Wellerson <wllrsn.c@gmail.com>
2023-06-28 23:53:01 -0300

    feat: api config

---
+++ packages/client/src/services/postLoginSession.ts
@@ -0,0 +1,27 @@
+import Api from "./api";
+
+type Data = {
+  email: string;
+  password: string;
+};
+
+type Props = {
+  data?: Data;
+};
+
+type Response = {
+  access_token: string;
+  message: string;
+};
+
+const postLoginSession = async (props: Props): Promise<Response> => {
+  const { data } = props;
+
+  const api = Api();
+
+  const response = await api.post<Response>("/auth/login", data);
+
+  return response.data;
+};
+
+export default postLoginSession;
