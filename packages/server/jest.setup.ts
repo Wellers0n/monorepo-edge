2135f3b0919117743e031ca99e07a7fb22af9b1d
Wellerson <wllrsn.c@gmail.com>
2023-08-13 13:32:46 -0300

    fix: reset data

--- packages/server/jest.setup.ts
+++ packages/server/jest.setup.ts
@@ -1,5 +1,8 @@
 import { Client } from 'pg';
 import { execSync } from 'child_process';
+import { Prisma, PrismaClient } from '@prisma/client';
+
+let client
 
 beforeAll(async () => {
   process.env.DATABASE_URL = process.env.DATABASE_TEST_URL;
@@ -9,21 +12,31 @@ beforeAll(async () => {
     `export DATABASE_URL=${process.env.DATABASE_TEST_URL} && npx prisma migrate deploy`,
     { stdio: 'inherit' },
   );
+
+  client = new Client({
+    connectionString: process.env.DATABASE_TEST_URL,
+  });
+  await client.connect();
 });
 
 beforeEach(async () => {
+  await client.query(`DROP SCHEMA IF EXISTS "test" CASCADE;`);
+  
   execSync(
-    `export DATABASE_URL=${process.env.DATABASE_TEST_URL} && npx prisma migrate reset -f`,
+    `export DATABASE_URL=${process.env.DATABASE_TEST_URL} && npx prisma migrate deploy`,
     { stdio: 'inherit' },
   );
+  // await client.query(`CREATE SCHEMA test;`);
+
+  
 });
 
 afterAll(async () => {
-  const client = new Client({
-    connectionString: process.env.DATABASE_TEST_URL,
-  });
+  // const client = new Client({
+  //   connectionString: process.env.DATABASE_TEST_URL,
+  // });
 
-  await client.connect();
+  
   await client.query(`DROP SCHEMA IF EXISTS "test" CASCADE`);
   await client.end();
 });
